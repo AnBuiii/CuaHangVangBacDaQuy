b90129ef16de256c99c5513e7be004188cc49204
AnBuiii <builehoaian2002@gmail.com>
2022-10-19 17:02:59 +0700

    add homeview

---
+++ CuaHangVangBacDaQuy/viewmodels/HomeViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CuaHangVangBacDaQuy.views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CuaHangVangBacDaQuy.viewmodels
+{
+    public class HomeViewModel : BaseViewModel
+    {
+
+        public ICommand LoadHomeView { get; set; }
+        public HomeViewModel()
+        {
+            LoadHomeView = new RelayCommand<HomeView>((p) => true, (p) => LoadingHomeView(p));
+        }
+
+        private void LoadingHomeView(HomeView view)
+        {
+            Console.WriteLine("asd");
+        }
+    }
+    
+
+
+}
--- CuaHangVangBacDaQuy/viewmodels/MainViewModel.cs
+++ CuaHangVangBacDaQuy/viewmodels/MainViewModel.cs
@@ -11,18 +11,36 @@ namespace CuaHangVangBacDaQuy.viewmodels
 {
     public class MainViewModel : BaseViewModel
     {
+
+        private object _dataTemplate;
+        public object DataTemplate
+        {
+            get => _dataTemplate;
+            set
+            {
+                _dataTemplate = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public HomeViewModel HomeViewModel { get; set; }
         public ICommand LoadedWindowCommand { get; set; }
         public bool isLoaded {get; set;} = false;
         public MainViewModel()
         {
-            LoadedWindowCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
-            {
-                isLoaded = true;
-                LoginWindow loginWindow = new LoginWindow();
-                loginWindow.ShowDialog();
-            });
-            
-            
+            InitNavBar();
+            _dataTemplate = HomeViewModel;
+            //LoadedWindowCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            //{
+            //    isLoaded = true;
+            //    LoginWindow loginWindow = new LoginWindow();
+            //    loginWindow.ShowDialog();
+            //});
+           
+        }
+        public void InitNavBar()
+        {
+            HomeViewModel = new HomeViewModel();
         }
     }
 }

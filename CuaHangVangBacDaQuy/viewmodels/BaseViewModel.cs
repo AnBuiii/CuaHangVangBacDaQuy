2c479a55be08caa5140dd3588e1e1b8cee4a5670
LightCode5794 <trantronghoang5794@gmail.com>
2022-10-23 02:35:24 +0700

    hơi rối :V

--- CuaHangVangBacDaQuy/viewmodels/BaseViewModel.cs
+++ CuaHangVangBacDaQuy/viewmodels/BaseViewModel.cs
@@ -13,11 +13,26 @@ namespace CuaHangVangBacDaQuy.viewmodels
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
+        protected virtual bool SetProperty<T>(ref T member, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(member, value))
+            {
+                return false;
+            }
+
+            member = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
+
+   
+
     class RelayCommand<T> : ICommand
     {
         private readonly Predicate<T> _canExecute;

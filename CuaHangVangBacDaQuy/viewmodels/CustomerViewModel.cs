2d9daf8d7eccafab50858c6da594884381d0ea1c
LightCode5794 <trantronghoang5794@gmail.com>
2022-10-31 14:25:37 +0700

    update add customer

--- CuaHangVangBacDaQuy/viewmodels/CustomerViewModel.cs
+++ CuaHangVangBacDaQuy/viewmodels/CustomerViewModel.cs
@@ -15,23 +15,49 @@ namespace CuaHangVangBacDaQuy.viewmodels
 
         #region Params
         private ObservableCollection<KhachHang> _CustomerList;
-        private ObservableCollection<KhachHang> CustomerList
+        public ObservableCollection<KhachHang> CustomerList
         {
             get  => _CustomerList; 
             set { _CustomerList = value; OnPropertyChanged(); }
         }
 
+        private bool _IsOpenAddCustomerDialog;
+        public bool IsOpenAddCustomerDialog
+        {
+            get { return _IsOpenAddCustomerDialog; }
+            set { _IsOpenAddCustomerDialog = value; OnPropertyChanged(); }
+        }
+
+        
+
+        private AddCustomerViewModel _addCustomerViewModel;
+        public AddCustomerViewModel addCustomerViewModel
+        {
+            get { return _addCustomerViewModel; }
+            set { _addCustomerViewModel = value; OnPropertyChanged(); }
+        }
+
         public ICommand LoadCustomerView { get; set; }
+        public ICommand AddCommand { get; set; }
+        public ICommand EditCommand { get; set; }
         #endregion
 
         public CustomerViewModel()
-        {
+        {   
+            IsOpenAddCustomerDialog=false;
             CustomerList = new ObservableCollection<KhachHang>(DataProvider.Ins.DB.KhachHangs);
+            AddCommand = new RelayCommand<CustomerView>((p) => true, p => { actionAddCustomer(); });
+            EditCommand = new RelayCommand<CustomerView>((p) => true, p => IsOpenAddCustomerDialog = true);
             
-            LoadCustomerView = new RelayCommand<CustomerView>((p) => true, (p) => loadCustomer(p));
+           // LoadCustomerView = new RelayCommand<CustomerView>((p) => true, (p) => loadCustomer(p));
         }
 
 
+        public void actionAddCustomer()
+        {   
+            addCustomerViewModel = new AddCustomerViewModel();
+            IsOpenAddCustomerDialog = true;
+        }
         void loadCustomer(CustomerView view)
         {
             

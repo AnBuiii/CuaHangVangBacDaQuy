f1c40a0fee723fa91524a7d35bb16e778c1e5d8f
AnBuiii <builehoaian2002@gmail.com>
2022-10-31 21:13:47 +0700

    add test project

---
+++ CuaHangVangBacDaQuyTests/viewmodels/ProductViewModelTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CuaHangVangBacDaQuy.viewmodels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CuaHangVangBacDaQuy.viewmodels.Tests
+{
+    [TestClass()]
+    public class ProductViewModelTests
+    {
+     
+        
+        [TestMethod()]
+        public void ProductViewModelTest()
+        {
+            
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LoadedTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddProductTest()
+        {
+            int a = 3;
+            int b = 4;
+
+            int c = a + b;
+            Assert.AreEqual(c, 7);
+        }
+
+        [TestMethod()]
+        public void EditProductTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteProductTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SaveAddTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
